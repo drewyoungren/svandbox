adfd33551d003bff8b56d8487e0cf0c21d60ef57
Drew C Youngren <dcy2@columbia.edu>
2024-07-04 18:32:48 -0400

    Fix grammar

--- src/routes/amp2024/5477/+page.svelte
+++ src/routes/amp2024/5477/+page.svelte
@@ -1,32 +1,53 @@
 <script>
-  let total = Math.ceil(Math.random()*100)
-  let moves = 0
+  let total = Math.ceil(Math.random() * 100);
+  let moves = 0;
 </script>
 
+<h1>Challenge 2.5: 54/77</h1>
 <div>
-   <p>
-      Starting with a random integer, will arbitrarily adding 54 or 77 repeatedly always result in a number whose last 2 digits are the same? 
-   </p>
-<p>Your total is:</p>
-  <h1 style="margin-left: 5%">{total}</h1>
+  <p>
+    Starting with a random integer, will arbitrarily adding 54 or 77 repeatedly
+    always result in a number whose last 2 digits are the same?
+  </p>
+  <p>Your total is:</p>
+  <div id="totdisplay">
+    <h1>{total}</h1>
+  </div>
 </div>
 
-{#if ((total % 100) % 11 == 0)}
-   <p><strong>You did it in {moves} moves!</strong></p>
+{#if (total % 100) % 11 == 0}
+  <p><strong>You did it in {moves} move{moves == 1 ? '' : 's'}!</strong></p>
 
-   <button on:click={() => {total = Math.ceil(Math.random()*100); moves = 0}}>Reset</button>
+  <button
+    on:click={() => {
+      total = Math.ceil(Math.random() * 100);
+      moves = 0;
+    }}>Reset</button
+  >
 {:else}
-   <div id="adders">
+  <div id="adders">
     {#each [54, 77] as num}
-       <button on:click={() => {total += num; moves += 1}}>{num}</button>
+      <button
+        on:click={() => {
+          total += num;
+          moves += 1;
+        }}>{num}</button
+      >
     {/each}
-   </div>
+  </div>
 {/if}
 
 <style>
-   #adders {
-      display: flex;
-      justify-content: space-around;
-      width: 10%;
-   }
+  #totdisplay {
+    display: flex;
+    justify-content: space-around;
+    width: 200px;
+    max-width: 100%;
+  }
+  #adders {
+    display: flex;
+    justify-content: space-around;
+    width: 200px;
+    max-width: 100%;
+  }
 </style>
