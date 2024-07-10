2c3548d0e18e0e8b0a73d8980e86c305a163fbda
Drew C Youngren <dcy2@columbia.edu>
2024-07-09 23:30:09 -0400

    Style

--- src/routes/amp2024/hungry_sneaky/+page.svelte
+++ src/routes/amp2024/hungry_sneaky/+page.svelte
@@ -16,6 +16,7 @@
    * @property {boolean} choc
    * @property {boolean} hungrySelect
    * @property {boolean} sneakySelect
+   * @property {number} shape
    */
 
   /**
@@ -51,6 +52,7 @@
           choc: true,
           hungrySelect: false,
           sneakySelect: false,
+          shape: Math.floor(Math.random() * 4),
         });
       }
     }
@@ -82,9 +84,12 @@
             score[1] += 1;
             node.sneakySelect = false;
             node.choc = false;
+            grid = [...grid];
           }, 500);
         });
     }
+    grid = [...grid];
+
     return goOn;
   }
 
@@ -96,6 +101,75 @@
    * @type Number
    */
   let sh = 2 * m * 100;
+
+  /**
+   *
+   * @param node {ChocNode}
+   */
+  const handleClick = (node) => {
+    let { i, j, choc, hungrySelect, sneakySelect } = node;
+    console.log('Got click', node);
+    if (turn % 3 == 1) {
+      if (choc && !hungrySelect) {
+        node.hungrySelect = true;
+        selected = [i, j];
+        turn += 1;
+      }
+    } else if (turn % 3 == 2) {
+      if (!clickLock) {
+        if (
+          choc &&
+          Math.abs(i - selected[0]) + Math.abs(j - selected[1]) == 1
+        ) {
+          node.hungrySelect = true;
+          clickLock = true;
+          setTimeout(() => {
+            turn += 1;
+            node.hungrySelect = false;
+            node.choc = false;
+            getGrid(selected[0], selected[1]).choc = false;
+            getGrid(selected[0], selected[1]).hungrySelect = false;
+            score[0] += 2;
+            selected = [];
+            clickLock = false;
+            grid = [...grid];
+          }, 500);
+        } else if (Math.abs(i - selected[0]) + Math.abs(j - selected[1]) == 0) {
+          selected = [];
+          node.hungrySelect = false;
+          turn -= 1;
+        }
+      }
+    } else {
+      if (!clickLock) {
+        node.sneakySelect = true;
+        clickLock = true;
+        setTimeout(() => {
+          node.sneakySelect = false;
+          node.choc = false;
+          score[1] += 1;
+          turn += 1;
+          clickLock = false;
+          grid = [...grid];
+        }, 500);
+      }
+    }
+    grid = [...grid];
+  };
+
+  /**
+   *
+   * @param i {number} row in grid
+   * @param j {number} col in grid
+   * @returns {string} string for svg polynomial
+   */
+  const makeTrap = (i, j) => {
+    let s = `${j * 100 + 30},${i * 100 + 10} `;
+    s += `${j * 100 + 70},${i * 100 + 10} `;
+    s += `${j * 100 + 90},${i * 100 + 90} `;
+    s += `${j * 100 + 10},${i * 100 + 90} `;
+    return s;
+  };
 
   onMount(() => console.log(grid));
 </script>
@@ -123,68 +197,53 @@
     style:height={(2 * m * 100).toString() + 'px'}
     style:width={(2 * n * 100).toString() + 'px'}
   >
-    {#each grid as { i, j, choc, sneakySelect, hungrySelect }}
-      <!-- <rect x={j * 100} y={i * 100} width="100" height="100" fill="black"
-      ></rect> -->
-      {#if choc}
+    {#each grid as node}
+      {#if node.choc}
         <!-- svelte-ignore a11y-click-events-have-key-events -->
         <!-- svelte-ignore a11y-no-static-element-interactions -->
-        <circle
-          cx={j * 100 + 50}
-          cy={i * 100 + 50}
-          r="45"
-          fill="#45200D"
-          on:click={() => {
-            if (turn % 3 == 1) {
-              if (choc && !hungrySelect) {
-                hungrySelect = true;
-                selected = [i, j];
-                turn += 1;
-              }
-            } else if (turn % 3 == 2) {
-              if (!clickLock) {
-                if (
-                  choc &&
-                  Math.abs(i - selected[0]) + Math.abs(j - selected[1]) == 1
-                ) {
-                  hungrySelect = true;
-                  clickLock = true;
-                  setTimeout(() => {
-                    turn += 1;
-                    hungrySelect = false;
-                    choc = false;
-                    getGrid(selected[0], selected[1]).choc = false;
-                    getGrid(selected[0], selected[1]).hungrySelect = false;
-                    score[0] += 2;
-                    selected = [];
-                    clickLock = false;
-                  }, 500);
-                } else if (
-                  Math.abs(i - selected[0]) + Math.abs(j - selected[1]) ==
-                  0
-                ) {
-                  selected = [];
-                  hungrySelect = false;
-                  turn -= 1;
-                }
-              }
-            } else {
-              if (!clickLock) {
-                sneakySelect = true;
-                clickLock = true;
-                setTimeout(() => {
-                  sneakySelect = false;
-                  choc = false;
-                  score[1] += 1;
-                  turn += 1;
-                  clickLock = false;
-                }, 500);
-              }
-            }
-          }}
-          stroke={hungrySelect ? 'yellow' : sneakySelect ? 'lightblue' : ''}
-          stroke-width="4"
-        ></circle>
+
+        {#if node.shape < 2}
+          <circle
+            cx={node.j * 100 + 50}
+            cy={node.i * 100 + 50}
+            r="45"
+            fill="#66350F"
+            on:click={() => handleClick(node)}
+            stroke={node.hungrySelect
+              ? 'yellow'
+              : node.sneakySelect
+                ? 'lightblue'
+                : ''}
+            stroke-width="4"
+          ></circle>
+        {:else if node.shape == 2}
+          <rect
+            x={node.j * 100 + 10}
+            y={node.i * 100 + 10}
+            height="80"
+            width="80"
+            fill="#45200D"
+            on:click={() => handleClick(node)}
+            stroke={node.hungrySelect
+              ? 'yellow'
+              : node.sneakySelect
+                ? 'lightblue'
+                : ''}
+            stroke-width="4"
+          ></rect>
+        {:else if node.shape == 3}
+          <polygon
+            points={makeTrap(node.i, node.j)}
+            fill="#9A7B4D"
+            on:click={() => handleClick(node)}
+            stroke={node.hungrySelect
+              ? 'yellow'
+              : node.sneakySelect
+                ? 'lightblue'
+                : ''}
+            stroke-width="4"
+          ></polygon>
+        {/if}
       {/if}
     {/each}
   </svg>
@@ -240,8 +299,23 @@
     max-width: 1000px;
   }
 
+  table {
+    font-family: Arial, Helvetica, sans-serif;
+    border-collapse: collapse;
+    font-size: larger;
+  }
+
+  td,
   th {
-    font-weight: bold;
+    border: 1px solid #ddd;
+    padding: 8px;
+  }
+  th {
+    padding-top: 12px;
+    padding-bottom: 12px;
+    text-align: left;
+    background-color: #04aa6d;
+    color: white;
   }
 
   td {
