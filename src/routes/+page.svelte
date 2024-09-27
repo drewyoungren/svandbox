cad630c5c64ac4680885b0fd26df425875082ea1
Drew C Youngren <dcy2@columbia.edu>
2024-09-26 20:32:58 -0400

    Add link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,4 +8,5 @@
     <li><a href="{base}/amp2024">AMP 2024</a></li>
     <li><a href="{base}/nutty-tower">Nutty Tower</a></li>
     <li><a href="{base}/bouncy">Bouncy House</a></li>
+    <li><a href="{base}/airhockey">Air Pockey</a></li>
 </ul>
